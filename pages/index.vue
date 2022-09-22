ff5f98c10a0f052f98ca05c7075bf089caf56eae
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2022-09-22 13:40:20 +0530

    base

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex flex-col h-screen">
+    <Header />
+    <div class="flex flex-row w-full h-full">
+      <Sidebar />
+      <div class="flex flex-col w-full h-full">
+        <Tagbar />
+        <Home />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
